cf0f1494899c30edaac0728fbf6a8c522a70ff64
dreamingpenguin <116635760+dreamingpinguina@users.noreply.github.com>
2026-01-05 17:49:52 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 'base' is set to './' to ensure assets load correctly on GitHub Pages
-  // regardless of whether it's at the root or in a subdirectory.
-  base: './',
+  base: '/mykitchen/', // Replace with your repository name
 })
